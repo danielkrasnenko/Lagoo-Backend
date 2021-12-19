e64dd4246d8ea951494861d886b4573f27158787
Daniel Krasnenko <krasnenko.daniil@list.ru>
2021-12-19 19:01:35 +0700

    Add serilog to the app for logging

--- Lagoo.Api/Program.cs
+++ Lagoo.Api/Program.cs
@@ -1,9 +1,11 @@
 using Lagoo.Api;
 using Lagoo.Infrastructure.Persistence;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Startup.ConfigureServices(builder.Services, builder.Configuration);
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
