ddb7d4879d073a7346e4531a4b5dac9feb526491
Daniel Krasnenko <krasnenko.daniil@list.ru>
2022-01-03 03:33:13 +0700

    Populate database with roles

---
+++ Lagoo.Infrastructure/Persistence/Migrations/20220102191150_PopulateUserRoles.cs
@@ -0,0 +1,24 @@
+﻿using Lagoo.Domain.Types;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lagoo.Infrastructure.Persistence.Migrations
+{
+    public partial class PopulateUserRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var roleNames = new List<string> { AppUserRole.Admin, AppUserRole.Manager, AppUserRole.User };
+            
+            foreach (var roleName in roleNames)
+            {
+                migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName, ConcurrencyStamp) Values ('{Guid.NewGuid()}', '{roleName}', '{roleName.Normalize().ToUpper()}', '{Guid.NewGuid()}')");
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
